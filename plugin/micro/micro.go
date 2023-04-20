e0371e1c1880f355539157d5ddd6a7d304b95e89
Presnyakov Aleksandr Evgenevich <aepresnyakov@mtsbank.ru>
2023-04-20 16:43:32 +0300

    replace to go.unistack.org/micro/v3

--- plugin/micro/micro.go
+++ plugin/micro/micro.go
@@ -15,10 +15,10 @@ import (
 // Paths for packages used by code generated in this file,
 // relative to the import_prefix of the generator.Generator.
 const (
-	apiPkgPath     = "github.com/unistack-org/micro/v3/api"
+	apiPkgPath     = "go.unistack.org/micro/v3/api"
 	contextPkgPath = "context"
-	clientPkgPath  = "github.com/unistack-org/micro/v3/client"
-	serverPkgPath  = "github.com/unistack-org/micro/v3/server"
+	clientPkgPath  = "go.unistack.org/micro/v3/client"
+	serverPkgPath  = "go.unistack.org/micro/v3/server"
 )
 
 func init() {
